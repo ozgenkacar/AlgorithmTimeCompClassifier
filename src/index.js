39f7f8a23ba37ec84374c2c3179e48ef2bda2f05
ozgenkacar <55004814+ozgenkacar@users.noreply.github.com>
2021-12-14 20:33:57 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 
 
+// reset the text area if the language is changed
+const dropdown = document.getElementById("languageOption");
+dropdown.onchange = function() {
+    let codeInput = document.getElementById("codeInput");
+    codeInput.value = "";
+
+}
 
 
+// when button is clicked, gets the text from the text area:
 const getBigOBtn = document.getElementById("RunButton");
 getBigOBtn.onclick = function() {
     const codeInput = document.getElementById("codeInput");
@@ -15,17 +23,14 @@ getBigOBtn.onclick = function() {
     resultShow.innerHTML = result;
 }
 
+
+// this function should parse the input of the code and determine the big O notation
+// params:
+//  code: The code input
 function parseInput(code) {
     // get the for statement
     let newlineSplit = code.split("\n")
     let forStatements = getForStatements(newlineSplit);
     let result = getBigONotation(forStatements);
     return result;
-}
-
-const dropdown = document.getElementById("languageOption");
-dropdown.onchange = function() {
-    let codeInput = document.getElementById("codeInput");
-    codeInput.value = "";
-
 }
